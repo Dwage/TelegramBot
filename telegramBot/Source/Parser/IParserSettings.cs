d9056cab302e550f2844a55a740e49be007b6a8b
Dwage <SuperMartinSuper@yandex.ru>
2021-09-27 21:20:22 +0400

    Added interface IParserSettings

---
+++ telegramBot/Source/Parser/IParserSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace telegramBot.Source.Parser
+{
+    interface IParserSettings
+    {
+        string BaseUrl { get; set; }
+
+        string PathSegment { get; set; }
+
+        string TagToParseBy { get; set; }
+    }
+}

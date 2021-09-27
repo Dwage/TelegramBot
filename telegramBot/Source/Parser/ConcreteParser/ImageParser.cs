b8b6e217772cfbe75839df019650c19f35fec3da
Dwage <SuperMartinSuper@yandex.ru>
2021-09-27 21:22:01 +0400

    Added image and text concrete parsers

---
+++ telegramBot/Source/Parser/ConcreteParser/ImageParser.cs
@@ -0,0 +1,33 @@
+﻿using AngleSharp.Html.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegramBot.Source.Parser.ConcreteParser
+{
+    class ImageParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document, IParserSettings settings)
+        {
+            List<string> list = new List<string>();
+
+            string searchOptions;
+            if (settings.TagToParseBy != null && settings.TagToParseBy != string.Empty)
+                searchOptions = $"{settings.TagToParseBy} > img";
+            else
+                searchOptions = "img";
+
+            var items = document.QuerySelectorAll(searchOptions);
+
+            foreach (var item in items)
+            {
+                list.Add(item.GetAttribute("src"));
+            }
+
+            return list.ToArray();
+        }
+    }
+    
+}
---
+++ telegramBot/Source/Parser/ConcreteParser/TextParser.cs
@@ -0,0 +1,22 @@
+﻿using AngleSharp.Html.Dom;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace telegramBot.Source.Parser.ConcreteParser
+{
+    class TextParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document, IParserSettings settings)
+        {
+            var list = new List<string>();
+            var items = document.QuerySelectorAll(settings.TagToParseBy);
+
+            foreach (var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}

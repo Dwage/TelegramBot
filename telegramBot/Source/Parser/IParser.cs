ddad25bff43d1e741ab298f3125cb348947fd749
Dwage <SuperMartinSuper@yandex.ru>
2021-09-27 21:19:57 +0400

    Added interface IParser

---
+++ telegramBot/Source/Parser/IParser.cs
@@ -0,0 +1,9 @@
+﻿using AngleSharp.Html.Dom;
+
+namespace telegramBot.Source.Parser
+{
+    interface IParser<T> where T : class
+    {
+        T Parse(IHtmlDocument document, IParserSettings settings);
+    }
+}

80495cd9920de0d11d3b5a9590bbc986bddbc2f5
Dwage <SuperMartinSuper@yandex.ru>
2021-09-27 21:28:44 +0400

    Added HtmlLoader which has a method for getting an html document

---
+++ telegramBot/Source/Parser/ConcreteParser/HtmlLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace telegramBot.Source.Parser
+{
+    class HtmlLoader
+    {
+        readonly HttpClient client;
+        readonly string url;
+
+        public HtmlLoader(IParserSettings settings)
+        {
+            client = new HttpClient();
+            url = $"{settings.BaseUrl}";
+        }
+
+        public async Task<string> GetSource()
+        {
+            var response = await client.GetAsync(url);
+            string source = null;
+
+            if (response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                source = await response.Content.ReadAsStringAsync();
+            }
+
+            return source;
+        }
+    }
+}

b82db85249200e45688c3e047eed20f7eeeddcef
Dwage <SuperMartinSuper@yandex.ru>
2021-09-27 21:29:36 +0400

    Added concrete parser settings

---
+++ telegramBot/Source/Parser/ConcreteParser/HtmlParserSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegramBot.Source.Parser.ConcreteParser
+{
+    class HtmlParserSettings : IParserSettings
+    {
+        public HtmlParserSettings(string url, string tagToParseBy)
+        {
+            TagToParseBy = tagToParseBy;
+            BaseUrl = url;
+        }
+
+        public string BaseUrl { get; set; }
+
+        public string PathSegment { get; set; }
+
+        public string TagToParseBy { get; set; }
+
+    }
+}
